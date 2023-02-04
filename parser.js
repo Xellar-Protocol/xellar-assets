55a6a529c39a5dda5b28e4ade8ae713c80b390dc
denditopratama <denditoprtm@gmail.com>
2023-02-04 21:54:30 +0700

    refetch

--- parser.js
+++ parser.js
@@ -20,39 +20,32 @@ const downloadImage = async (url, image_path) => {
 
 }
 
-const sleeps = async () => await new Promise(r => setTimeout(r, 5000));
+const sleeps = async () => await new Promise(r => setTimeout(r, 4000));
 
 
-fs.readFile('./idlist.json', 'utf8', async function (err, data) {
-    const obj = JSON.parse(data);
-    let count = 0;
-    let queryParam = [];
-    // for (var i = count; i < 100; i++) {
-    //     queryParam.push(obj[i]['id'])
-    // }
+(async () => {
+    const files = fs.readdirSync('./assets')
+    //12402
     var latest = JSON.parse(fs.readFileSync('./record.json', 'utf8'));
-    for (var i = latest['latest_step'] ?? 0; i < obj.length; i++) {
-        count++;
+    for (var i = latest['latest_step'] ?? 0; i < files.length; i++) {
+        var record = JSON.parse(fs.readFileSync('./record.json', 'utf8'));
         let coinData;
-        let tempError = [];
-        console.log(obj[i]['id'])
-        //CG
+        let tempError = record['errorList'];
         try {
-            coinData = await axios.get(`https://api.coingecko.com/api/v3/coins/${obj[i]['id']}?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
+            coinData = await doFetch(files[i])
         } catch (e) {
-            tempError = latest['errorList'];
+            console.log(`missing coin / token data ${files[i]}`)
+            console.log(e)
             tempError.push({
-                "id": obj[i]['id']
+                "id": files[i]
             })
-            console.log('missing coin / token data')
+            fs.writeFileSync('./record.json', JSON.stringify({
+                "latest_step": i,
+                "errorList": tempError
+            }))
             continue;
         }
-        let { id, name, symbol, description, links, image } = coinData.data;
-        //CMC
-        // let coinData = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${obj[i]['id']}`, { headers: { "X-CMC_PRO_API_KEY": "161bfd50-6175-4e3c-87cc-8202a5882e44" } })
-        // let { data } = coinData.data;
-
-        await fs.promises.mkdir(`./assets/${id}`, { recursive: true })
+        let { id, name, symbol, description, links, image, detail_platforms } = coinData;
         fs.writeFileSync('./record.json', JSON.stringify({
             "latest_step": i,
             "errorList": tempError
@@ -63,18 +56,87 @@ fs.readFile('./idlist.json', 'utf8', async function (err, data) {
             "symbol": symbol,
             "description": description.en.replace(/\s+/g, ' ').trim(),
             "links": links.homepage[0],
-            "logo": `https://raw.githubusercontent.com/Xellar-Protocol/xellar-assets/master/assets/${id}/logo.png`
+            "logo": `https://raw.githubusercontent.com/Xellar-Protocol/xellar-assets/master/assets/${id}/logo.png`,
+            "detail_platform": detail_platforms
         }));
-        try {
-            await downloadImage(image.large, `./assets/${id}/logo.png`);
-        } catch (e) {
-            console.log('missing  image')
-            await downloadImage("https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579", `./assets/${id}/logo.png`);
-        }
+        console.log('\x1b[33m%s\x1b[0m', `done ${id} -> ${i}/${files.length}`);
         await sleeps()
-        console.log('\x1b[33m%s\x1b[0m', `done -> ${i}/${obj.length}`);
     }
+})()
+
+async function doFetch(Coinid) {
+    try {
+        var coinData = await axios.get(`https://api.coingecko.com/api/v3/coins/${Coinid}?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
+        let { id, name, symbol, description, links, image, detail_platforms } = coinData.data;
+        return {
+            id, name, symbol, description, links, image, detail_platforms
+        }
+    } catch (e) {
+        console.log(`retrying ${Coinid}`)
+        await sleeps()
+        var coinData = await axios.get(`https://api.coingecko.com/api/v3/coins/${Coinid}?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`).catch((e) => {
+            throw e.response.statusText
+        })
+        let { id, name, symbol, description, links, image, detail_platforms } = coinData.data;
+        return {
+            id, name, symbol, description, links, image, detail_platforms
+        }
+    }
+}
+
+// fs.readFile('./idlist.json', 'utf8', async function (err, data) {
+//     const obj = JSON.parse(data);
+//     let count = 0;
+//     let queryParam = [];
+//     // for (var i = count; i < 100; i++) {
+//     //     queryParam.push(obj[i]['id'])
+//     // }
+//     var latest = JSON.parse(fs.readFileSync('./record.json', 'utf8'));
+//     for (var i = latest['latest_step'] ?? 0; i < obj.length; i++) {
+//         count++;
+//         let coinData;
+//         let tempError = [];
+//         console.log(obj[i]['id'])
+//         //CG
+//         try {
+//             coinData = await axios.get(`https://api.coingecko.com/api/v3/coins/${obj[i]['id']}?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
+//         } catch (e) {
+//             tempError = latest['errorList'];
+//             tempError.push({
+//                 "id": obj[i]['id']
+//             })
+//             console.log('missing coin / token data')
+//             continue;
+//         }
+//         let { id, name, symbol, description, links, image, detail_platform } = coinData.data;
+//         //CMC
+//         // let coinData = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${obj[i]['id']}`, { headers: { "X-CMC_PRO_API_KEY": "161bfd50-6175-4e3c-87cc-8202a5882e44" } })
+//         // let { data } = coinData.data;
+
+//         await fs.promises.mkdir(`./assets/${id}`, { recursive: true })
+//         fs.writeFileSync('./record.json', JSON.stringify({
+//             "latest_step": i,
+//             "errorList": tempError
+//         }))
+//         fs.writeFileSync(`./assets/${id}/info.json`, JSON.stringify({
+//             "name": name,
+//             "id": id,
+//             "symbol": symbol,
+//             "description": description.en.replace(/\s+/g, ' ').trim(),
+//             "links": links.homepage[0],
+//             "logo": `https://raw.githubusercontent.com/Xellar-Protocol/xellar-assets/master/assets/${id}/logo.png`,
+//             "detail_platform": detail_platform
+//         }));
+//         try {
+//             await downloadImage(image.large, `./assets/${id}/logo.png`);
+//         } catch (e) {
+//             console.log('missing  image')
+//             await downloadImage("https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579", `./assets/${id}/logo.png`);
+//         }
+//         await sleeps()
+//         console.log('\x1b[33m%s\x1b[0m', `done -> ${i}/${obj.length}`);
+//     }
 
 
-});
+// });
 
