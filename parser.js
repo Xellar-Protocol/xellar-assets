2bae6522eb423b487653ef55efc204ae371c115b
denditopratama <denditoprtm@gmail.com>
2023-09-18 18:08:43 +0700

    add tokenlist2.json

--- parser.js
+++ parser.js
@@ -1,7 +1,13 @@
 const fs = require('fs')
 const axios = require('axios');
 const { resolve } = require('path');
-const { isEmpty } = require('lodash');
+const { isEmpty, _ } = require('lodash');
+
+
+
+const exeptionList = [
+    "ethereum",
+]
 
 const downloadImage = async (url, image_path) => {
     return new Promise(async (resolve, reject) => {
@@ -23,30 +29,43 @@ const downloadImage = async (url, image_path) => {
 
 const sleeps = async () => await new Promise(r => setTimeout(r, 5500));
 
-const findNative = (input) => {
-    let nativeList = ['ethereum', 'binancecoin', 'matic-network', 'avalanche-2', 'fantom', 'tomochain', 'harmony', 'moonbeam', 'moonriver', 'kucoin-shares', 'kava', 'arbitrum'];
+const findNativeByID = (input) => {
+    let nativeList = ['ethereum', 'binancecoin', 'matic-network', 'avalanche-2', 'fantom'];
+    //'tomochain', 'harmony', 'moonbeam', 'moonriver', 'kucoin-shares', 'kava',
     return !isEmpty(nativeList.filter((x) => x == input))
 }
 
 
 
-const constructTokenList = () => {
+const constructTokenList = ({
+    fileName = 'tokenlist.json'
+}) => {
     let constructJSON = [];
     const files = fs.readdirSync('./assets')
     for (var i = 0; i < files.length; i++) {
-        var info = JSON.parse(fs.readFileSync(`./assets/${files[i]}/info.json`, 'utf8'));
-
-        constructJSON.push({
-            "id": info.id,
-            "name": info.name,
-            "symbol": info.symbol,
-            "logo": info.logo,
-            "is_native": findNative(info.id),
-            "detail_platform": info.detail_platform
-        })
-        console.log('\x1b[33m%s\x1b[0m', `done ${files[i]} -> ${i}/${files.length}`);
+        try {
+            var info = JSON.parse(fs.readFileSync(`./assets/${files[i]}/info.json`, 'utf8'));
+            let isNative = findNativeByID(info.id)
+            let tempNative = info.detail_platform['native'] ?? {};
+            if (isNative) {
+                info.detail_platform['native'] = [
+                    tempNative
+                ];
+            }
+            constructJSON.push({
+                "id": info.id,
+                "name": info.name,
+                "symbol": info.symbol,
+                "logo": info.logo,
+                "is_native": isNative,
+                "detail_platform": info.detail_platform
+            })
+            console.log('\x1b[33m%s\x1b[0m', `done ${files[i]} -> ${i}/${files.length}`);
+        } catch (e) {
+            console.log(e)
+        }
     }
-    fs.writeFileSync('./tokenlist.json', JSON.stringify(constructJSON))
+    fs.writeFileSync(`./${fileName}`, JSON.stringify(constructJSON))
 }
 
 
@@ -128,18 +147,26 @@ async function doFetch(Coinid) {
 const fetchAllTokens = async () => {
     fs.readFile('./idlist.json', 'utf8', async function (err, data) {
         const obj = JSON.parse(data);
+
+
         let count = 0;
         let queryParam = [];
-        // for (var i = count; i < 100; i++) {
-        //     queryParam.push(obj[i]['id'])
-        // }
+
         var latest = JSON.parse(fs.readFileSync('./record.json', 'utf8'));
         for (var i = latest['latest_step'] ?? 0; i < obj.length; i++) {
+
+            try {
+                var info = JSON.parse(fs.readFileSync(`./assets/${obj[i]['id']}/info.json`, 'utf8'));
+                console.log(`skip ${obj[i]['id']}`)
+            }
+            catch (e) {
+                console.log('\x1b[33m%s\x1b[0m', `fetching -> ${i}/${obj.length}`);
+            }
+
             count++;
             let coinData;
             let tempError = [];
-            console.log(obj[i]['id'])
-            //CG
+
             try {
                 coinData = await axios.get(`https://api.coingecko.com/api/v3/coins/${obj[i]['id']}?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
             } catch (e) {
@@ -151,9 +178,7 @@ const fetchAllTokens = async () => {
                 continue;
             }
             let { id, name, symbol, description, links, image, detail_platforms } = coinData.data;
-            //CMC
-            // let coinData = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${obj[i]['id']}`, { headers: { "X-CMC_PRO_API_KEY": "161bfd50-6175-4e3c-87cc-8202a5882e44" } })
-            // let { data } = coinData.data;
+
 
             await fs.promises.mkdir(`./assets/${id}`, { recursive: true })
             fs.writeFileSync('./record.json', JSON.stringify({
@@ -198,6 +223,12 @@ const fetchAllTokens = async () => {
 }
 
 
+const getIdList = async () => {
+    const result = await axios.get('https://api.coingecko.com/api/v3/coins/list');
+    fs.writeFileSync('./idlist.json', JSON.stringify(result.data));
+}
+
+
 
 (async () => {
     // STEP //
@@ -205,6 +236,13 @@ const fetchAllTokens = async () => {
     // get idlist.json
     // fetchAllTokens
     // fetchAllErrorTokenDetailData -->> fill error data
-    // constructTokenList
-    fetchAllTokens();
+    // getIdList().then(() => {
+    //     fetchAllTokens();
+    // })
+    constructTokenList({
+        fileName: "tokenlist2.json"
+    })
+
 })()
+
+
