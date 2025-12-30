183e28f6fae9afff2e2eba73cc432b164794bf63
vickrylazuardi <vickrylazuardi@gmail.com>
2025-12-30 19:35:31 +0700

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import { constructTokenList } from './constructTokenlist.js';
 import { fetchTokens } from './fetchTokens.js';
 import { getCoinsWithMarketCap } from './getCoinsWithMarketCap.js';
 
 const service = async () => {
-    // const coins = await getCoinsWithMarketCap();
-    // await fetchTokens(coins);
-
-    constructTokenList({
-        fileName: "tokenlist2.json",
-        isTop50: false
-    });
+    const coins = await getCoinsWithMarketCap();
+    await fetchTokens(coins);
 }
 
 service();
