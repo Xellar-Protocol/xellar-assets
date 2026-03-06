a0d91298e6d1706359bf92c5c1e11712a0e79268
vickrylazuardi <vickrylazuardi@gmail.com>
2026-03-06 18:53:21 +0700

    feat: Add morph-l2 to supported networks and native currency configuration.

--- src/supportedNetworkAndNativeCurrency.js
+++ src/supportedNetworkAndNativeCurrency.js
@@ -1,8 +1,8 @@
 export const nativeCurrency = {
-    'bitcoin': 
-        {
-            "network_id": "bitcoin"
-        },
+    'bitcoin':
+    {
+        "network_id": "bitcoin"
+    },
     'ethereum': [
         {
             "network_id": "ethereum"
@@ -18,40 +18,43 @@ export const nativeCurrency = {
         },
         {
             "network_id": "lisk"
+        },
+        {
+            "network_id": "morph-l2"
         }
     ],
-    'binancecoin': 
-        {
-            "network_id": "binance-smart-chain"
-        },
-    'polygon-ecosystem-token': 
-        {
-            "network_id": "polygon-pos"
-        },
-    'avalanche-2': 
-        {
-            "network_id": "avalanche"
-        },
-    'fantom': 
-        {
-            "network_id": "fantom"
-        },
-    'solana': 
-        {
-            "network_id": "solana"
-        },
-    'immutable-x': 
-        {
-            "network_id": "immutable"
-        },
-    'okb': 
-        {
-            "network_id": "x-layer"
-        },
+    'binancecoin':
+    {
+        "network_id": "binance-smart-chain"
+    },
+    'polygon-ecosystem-token':
+    {
+        "network_id": "polygon-pos"
+    },
+    'avalanche-2':
+    {
+        "network_id": "avalanche"
+    },
+    'fantom':
+    {
+        "network_id": "fantom"
+    },
+    'solana':
+    {
+        "network_id": "solana"
+    },
+    'immutable-x':
+    {
+        "network_id": "immutable"
+    },
+    'okb':
+    {
+        "network_id": "x-layer"
+    },
     'tron':
-        {
-            "network_id": "tron"
-        }
+    {
+        "network_id": "tron"
+    }
 }
 
-export const supportedNetwork = ["bitcoin", "ethereum", "polygon-pos", "binance-smart-chain", "avalanche", "fantom", "optimistic-ethereum", "arbitrum-one", "base", "solana", "lisk", "immutable", "x-layer", 'tron', '', 'native']
+export const supportedNetwork = ["bitcoin", "ethereum", "polygon-pos", "binance-smart-chain", "avalanche", "fantom", "optimistic-ethereum", "arbitrum-one", "base", "solana", "lisk", "immutable", "x-layer", 'tron', 'morph-l2', '', 'native']
